6e565d6766a4bc45496e48ea5ac76a90d3f1e05d
liutsing <liutsingluo@gmail.com>
2022-04-01 22:29:53 +0800

    ~

--- vue/geektime/vue.config.js
+++ vue/geektime/vue.config.js
@@ -2,4 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   runtimeCompiler: true,
+  configureWebpack: {
+    externals:{
+      vue: 'Vue'
+    }
+  }
 });
