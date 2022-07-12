9f6325f40b847492d33eb9be8d3ec660c3cbef39
Liutsing <liutsingluo@163.com>
2022-07-13 03:36:33 +0800

    feat: try vue-loader plugin

--- vue/geektime/src/views/i18n/index.vue
+++ vue/geektime/src/views/i18n/index.vue
@@ -1,8 +1,59 @@
 <template>
-  <div>{{$t('hello')}}</div>
+    <div>
+        <section>
+            <p>模板:</p>
+            <div>{{ $t('hello') }}</div>
+            <li class="item-container">
+                <span>{{ $t('billing.time-fee') }}:</span>
+                <span>{{ ' ' }}{{ currencySign }}</span>
+                <span>{{ ' ' }}{{ item.timePrice }}/{{ $t('unit.min') }}</span>
+            </li>
+            <p>模板-多个key:</p>
+            <span>
+                {{ $t('gunMonitor.gunNumber') }}: {{ currencySign
+                }}{{ $t('gun') }}
+            </span>
+            <p>组件:</p>
+            <a-button type="primary">{{ $t('hello') }}</a-button>
+            <p>输入项:</p>
+            <input :placeholder="$t('hello')" />
+            <textarea :placeholder="$t('hello')" value="111"></textarea>
+        </section>
+        <section>
+            <p>运行时:</p>
+            <a-table :columns="columns"></a-table>
+        </section>
+    </div>
 </template>
 <script>
 export default {
-
+    computed: {
+        columns() {
+            return [
+                {
+                    dataIndex: 'a',
+                    title: this.$t('hello'),
+                },
+                {
+                    dataIndex: 'aa',
+                    title: this.$t('bb'),
+                },
+            ]
+        },
+    },
+    data() {
+        return {
+            obj: {},
+            currencySign: 0,
+            item: {
+                timePrice: 0,
+            },
+        }
+    },
 }
 </script>
+<style lang="less" scoped>
+p {
+    margin: 2em;
+}
+</style>
--- vue/geektime/vue.config.js
+++ vue/geektime/vue.config.js
@@ -13,7 +13,66 @@ module.exports = defineConfig({
     //   sprite: {}, // Pass options to svg-sprite-loader
     // }
     // }
+
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        options.compilerOptions.modules = [
+          {
+            transformNode: (el) => {
+              try {
+                // 静态渲染
+                if (el.tag === 'input' || el.tag === 'textarea') {
+                  const key = el.rawAttrsMap[':placeholder'].value
+                  el.attrsList.push({
+                    name: `data-i18n`, value: key
+                  })
+                }
+                if (el.children.length === 1) {
+                  const child = el.children[0]
+                  if (child.type === 2) {
+                    const text = child.text
+                    // FIXME 提取出 {{$t(<key>)}} 正则
+                    const reg = new RegExp(`\{\{\\s?\\$t\\('([a-zA-Z0-9\\-\\.]+)'\\)\\s?\}\}`, 'gm')
+                    const matches = text.match(reg)
+                    if (matches) {
+                      const values = []
+                      for (let i = 0; i < matches.length; i++) {
+                        const _match = matches[i];
+                        const reg1 = new RegExp(`\\$t\\(['"](.+)['"]\\)`)
+                        const matches1 = _match.match(reg1)
+                        if (matches1) {
+                          const key = matches1[1]
+                          values.push(key)
+                          // 处理input, 组件, 占位符, 枚举类 -> ant-design-vue组件内列名
+                        }
+                      }
+                      if (values.length > 1) {
+                        el.attrsList.push({
+                          name: `data-i18n`, value: values
+                        })
+                      } else {
+                        el.attrsList.push({
+                          name: `data-i18n`, value: values[0]
+                        })
+                      }
+                    }
+                  }
+                }
+                // a-table列名, a-description.item
+              } catch (error) {
+                console.log(error.message)
+              }
+            }
+          }
+        ]
+        return options
+      })
+  }
+
   // css: {
   //   loaderOptions: {
   //     postcss: {
