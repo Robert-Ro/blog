480d82f98457014685175c567c7808a4559b5aac
liutsing <liutsingluo@gmail.com>
2022-07-22 11:44:22 +0800

    feat: 在js层面的处理
    feat: ~

---
+++ vue/vue3-taste/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>vue3 Features Demo</div>
+    <RouterView />
+    <RouterLink to="/">home</RouterLink>
+    <RouterLink to="/about">about</RouterLink>
+</template>
+
+<script setup lang="ts"></script>
